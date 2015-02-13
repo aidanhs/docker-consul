85a635f5a707547014a56463105e82447e5c8479
Aidan Hobson Sayers <aidanhs@cantab.net>
2015-02-13 22:21:06 +0000

    Better logging

--- reload-consul.sh
+++ reload-consul.sh
@@ -4,20 +4,24 @@ set -o errexit
 cd /config
 
 CURCOMMIT="$(git rev-parse HEAD)"
-NEWCOMMIT="$(curl http://localhost:8500/v1/kv/consul/commitish?raw)"
+NEWCOMMIT="$(curl -sS http://localhost:8500/v1/kv/consul/commitish?raw)"
 
 echo "Considering '$CURCOMMIT', '$NEWCOMMIT'"
 
 if [ "$NEWCOMMIT" = "" -o "$NEWCOMMIT" = "No cluster leader" ]; then
     # Commit hash hasn't been put into kv store yet
+    echo "Cluster not formed or no new commit set"
     exit 0
-if [ "${#CURCOMMIT}" != "${#NEWCOMMIT}" ]; then
+elif [ "${#CURCOMMIT}" != "${#NEWCOMMIT}" ]; then
     # A commit hash isn't the right length
+    echo "Invalid commit hash length"
     exit 1
 elif [ "$CURCOMMIT" = "$NEWCOMMIT" ]; then
     # Already on the commit
+    echo "Already on correct commit"
     exit 0
 else
+    echo "Updating consul config"
     # These two are in case the initial build was a dirty git repo
     git clean -f -x -d
     git reset --hard
